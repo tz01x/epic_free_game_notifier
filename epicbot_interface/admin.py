c54eb94420bdee3fb3d7868f9bcbadc36711f3b6
tumzied_ <50454992+tz01x@users.noreply.github.com>
2023-01-16 16:57:09 +0000

    add manual action to fetch data and notify all users

--- epicbot_interface/admin.py
+++ epicbot_interface/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Subscribers,JobLogs
+from epicbot_interface.epic_bot.fetch_promotionalOffers_games import fetch_promo_game
 
+@admin.action(description='Fetch New Data and Notify All')
+def fetch_new_data_action(modeladmin, request, queryset):
+    fetch_promo_game()
+    
 
 @admin.register(JobLogs)
 class JobLogsAdminModel(admin.ModelAdmin):
@@ -12,3 +17,4 @@ class SubscribersAdminModel(admin.ModelAdmin):
     search_fields=['email',]
     list_filter=['created_at']
     list_display = ['id','email','created_at','is_active']
+    actions = [fetch_new_data_action]
