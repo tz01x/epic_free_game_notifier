b558b46891fc379de8652d27aaa11abea50213c9
tz01x <abudr963rahman@gmail.com>
2023-01-21 18:45:22 +0600

    add webhook route and basic funcs

---
+++ lib.py
@@ -0,0 +1,71 @@
+import os
+import requests
+
+def verify_token(mode, token, challenge):
+    VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+    if mode and token:
+        # Check the mode and token sent are correct
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            # // Respond with 200 OK and challenge token from the request
+            print("WEBHOOK_VERIFIED")
+            return challenge
+    raise Exception("not verify")
+
+
+def process_event(body):
+    if body.get('object') == 'page':
+        for entry in body.get('entry'):
+            webhook_event = entry['messaging'][0]
+            sender_psid = webhook_event\
+                .get('sender')\
+                .get('id')
+            if (webhook_event.get("message")):
+                handleMessage(sender_psid, webhook_event['message'])
+            elif webhook_event.get("postback"):
+                handlePostback(sender_psid, webhook_event['postback'])
+                pass
+        return True
+    return False
+
+
+def handleMessage(sender_psid, received_message):
+    response = {}
+    if received_message.get('text'):
+        response = {
+            "text": f"you sent the message: {received_message['text']} Now send me an image!"
+        }
+    elif received_message.get("attachments"):
+        attachment_urls = received_message["attachments"][0]["payload"]["url"]
+        response = {
+            "attachment":{
+            "type":"image", 
+            "payload":{
+                "url":attachment_urls, 
+                "is_reusable":True
+            }
+            },
+            "text":"is this the image"
+            
+        }
+    callSendAPI(sender_psid, response)
+
+
+def handlePostback(sender_psid, received_postback):
+    pass
+
+
+def callSendAPI(sender_psid, response):
+    request_body = {
+        "recipient": {
+            "id": sender_psid,
+        },
+        "message": response,
+    }
+    PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
+    url = f"https://graph.facebook.com/v2.6/me/messages?access_token={PAGE_ACCESS_TOKEN}"
+    response = requests.post(url, json = request_body)
+    if response.status_code:
+        print("message sent")
+    else:
+        print("error "+ response.text)
+    
--- server.py
+++ server.py
@@ -1,12 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask,request
 from dotenv import load_dotenv
+from lib import process_event, verify_token
 # Load .env file using:
 load_dotenv()
 app = Flask(__name__)
  
-@app.route("/")
-def hello():
-  return "Hello World!"
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        mode = request.args.get('hub.mode')
+        token = request.args.get('hub.verify_token')
+        challenge = request.args.get('hub.challenge')
+        try:
+            return verify_token(mode, token, challenge), 200
+        except Exception as e:
+            # // Responds with '403 Forbidden' if verify tokens do not match
+            return b'',403
+    elif request.method == 'POST':
+        body = request.get_json()
+        if process_event(body):
+            return "EVENT_RECEIVED", 200
+        return b"", 404 
  
 app.run()
